516e4ae38079426286de20a5284dd042b254de75
Nhung Nguyen <Nhung Nguyen>
2016-11-08 18:52:57 +0700

    update

--- app/src/main/java/com/example/a51202_000/testbug/RegisterActivity.java
+++ app/src/main/java/com/example/a51202_000/testbug/RegisterActivity.java
@@ -1,13 +1,181 @@
 package com.example.a51202_000.testbug;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 public class RegisterActivity extends AppCompatActivity {
+    private EditText edtEmail, edtPass, edtRepass;
+    private Button btnRegister, btnLogin;
+    private ProgressDialog progressDialog;
+
+    private static String RegisterURL = "http://192.168.1.112:3000/api/user";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        edtEmail = (EditText) findViewById(R.id.edtEmailRe);
+        edtPass = (EditText) findViewById(R.id.edtPasswordRe);
+        edtRepass = (EditText) findViewById(R.id.edtRePasswordRe);
+
+        btnLogin = (Button) findViewById(R.id.btnLoginRe);
+        btnRegister = (Button) findViewById(R.id.btnRegisterRe);
+
+        btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                String uName = edtEmail.getText().toString();
+                String uPass = edtPass.getText().toString();
+                String uRePass = edtRepass.getText().toString();
+
+                if(uName.trim().equals("") ||uPass.trim().equals("")||uRePass.trim().equals("")) {
+                    Toast.makeText(getApplicationContext()," Hãy điền đầy đủ thông tin", Toast.LENGTH_LONG).show();
+                }
+                else if(!uRePass.equals(uPass))
+                {
+                    Toast.makeText(getApplicationContext(),"Password nhập lại không đúng", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    new connectServerRe().execute(RegisterURL);
+                }
+
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(RegisterActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
+    private class connectServerRe extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            ProgressDialog progressDialog = new ProgressDialog(RegisterActivity.this);
+            progressDialog.setMessage(" Kiểm tra đăng Ky... ");
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                return SetData(params[0]);
+            } catch (IOException ex){
+                return "Network Error ";
+            } catch (JSONException ex) {
+                return "Invalid data!";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            if (progressDialog != null) {
+                progressDialog.dismiss();
+            }
+            try{
+                if(checkLogin(result) == true) {
+                    Toast.makeText(getApplicationContext(), "Register sucess", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(RegisterActivity.this, MainTabActivity.class);
+                    startActivity(intent);
+                }
+                else
+                    Toast.makeText(getApplicationContext(),"Register fail", Toast.LENGTH_LONG).show();
+            }catch(JSONException ex)
+            {
+                Toast.makeText(getApplicationContext(),"Register fail ex", Toast.LENGTH_LONG).show();
+            }
+
+        }
+
+        private String SetData(String urlPath) throws IOException, JSONException {
+            StringBuilder reSult = new StringBuilder();
+            BufferedWriter bufferedWriter = null;
+            BufferedReader bufferedReader = null;
+
+            try {
+                JSONObject dataInsert = new JSONObject();
+                //create JSONdata to send to server
+                dataInsert.put("username",edtEmail.getText());
+                dataInsert.put("password",edtPass.getText());
+                //txtLoginView.setText(urlPath);
+
+
+
+                //initialize and config request , then connect the server.
+                URL url = new URL(urlPath);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setReadTimeout(10000); //milliseconds
+                urlConnection.setConnectTimeout(10000);
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setDoOutput(true); //enable output data
+                urlConnection.setRequestProperty("Content-Type","application/json"); //set header
+                urlConnection.connect();
+
+                //write data to server
+                OutputStream outputStream = urlConnection.getOutputStream();
+                bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream));
+                bufferedWriter.write(dataInsert.toString());
+                bufferedWriter.flush();
+
+                //read data response from server
+
+                InputStream inputStream = urlConnection.getInputStream();
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                if((line = bufferedReader.readLine())!=null) {
+                    reSult.append(line);
+                }
+                // JSONObject resultJSON= new JSONObject(reSult.toString());
+                //message = resultJSON.getString("message");
+
+            } finally {
+                if(bufferedWriter !=null) {
+                    bufferedWriter.close();
+                }
+                if(bufferedReader !=null) {
+                    bufferedReader.close();
+                }
+            }
+
+            return reSult.toString();
+        }
+    }
+
+    Boolean checkLogin(String reSult ) throws JSONException{
+        JSONObject resultJSON= new JSONObject(reSult.toString());
+        String flag = resultJSON.getString("CODE");
+        if (flag.equals("SUCCESS")) return true;
+        else return false;
+
+    }
+
 }
