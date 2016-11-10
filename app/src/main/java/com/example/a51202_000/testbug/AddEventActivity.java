ed0800d848d81bc99ab7a4c2aa710aebb522e819
51202574 <51202574@hcmut.edu.vn>
2016-11-11 01:55:39 +0700

    commit layout add event

--- app/src/main/java/com/example/a51202_000/testbug/AddEventActivity.java
+++ app/src/main/java/com/example/a51202_000/testbug/AddEventActivity.java
@@ -2,17 +2,25 @@ package com.example.a51202_000.testbug;
 
 import android.content.Intent;
 import android.graphics.Color;
+import android.location.Location;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.view.View.OnClickListener;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
+import com.google.android.gms.common.GooglePlayServicesRepairableException;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlacePicker;
+import com.google.android.gms.maps.model.LatLng;
 import com.wdullaer.materialdatetimepicker.Utils;
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
 import com.wdullaer.materialdatetimepicker.time.RadialPickerLayout;
@@ -21,13 +29,20 @@ import com.wdullaer.materialdatetimepicker.time.TimePickerDialog;
 import java.util.Calendar;
 import java.util.TimeZone;
 
+import layout.FriendFrament;
+import layout.HomeFragment;
+import layout.RouteMapEventFragment;
+
 public class AddEventActivity extends AppCompatActivity implements DatePickerDialog.OnDateSetListener, TimePickerDialog.OnTimeSetListener {
 
     private DatePicker datePicker;
     private Calendar calendar;
     private EditText dateView, timeView;
+    private EditText StartAddress, EndAddress;
     private int year, month, day;
-
+    private int PLACE_PICKER_REQUEST = 101;
+    private int PLACE_DESTINATION_PICKER_REQUEST = 102;
+    Button OkButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,6 +54,10 @@ public class AddEventActivity extends AppCompatActivity implements DatePickerDia
 
         dateView = (EditText) findViewById(R.id.DateTimePickerEditText);
         timeView = (EditText) findViewById(R.id.TimePickerEditText);
+
+        StartAddress = (EditText) findViewById(R.id.StartAddress);
+        EndAddress = (EditText) findViewById(R.id.EndtAddress);
+
         calendar = Calendar.getInstance();
         year = calendar.get(Calendar.YEAR);
 
@@ -78,9 +97,82 @@ public class AddEventActivity extends AppCompatActivity implements DatePickerDia
                 tpd.show(getFragmentManager(), "Timepickerdialog");
             }
         });
+
+        StartAddress.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                PlacePicker.IntentBuilder buider = new PlacePicker.IntentBuilder();
+
+                Intent intent;
+                try {
+                    intent = buider.build(getApplicationContext());
+                    startActivityForResult(intent, PLACE_PICKER_REQUEST);
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesRepairableException ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+        });
+        EndAddress.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                PlacePicker.IntentBuilder buider = new PlacePicker.IntentBuilder();
+
+                Intent intent;
+                try {
+                    intent = buider.build(getApplicationContext());
+                    startActivityForResult(intent, PLACE_DESTINATION_PICKER_REQUEST);
+                } catch (GooglePlayServicesNotAvailableException e) {
+                    e.printStackTrace();
+                } catch (GooglePlayServicesRepairableException ex) {
+                    ex.printStackTrace();
+                }
+
+            }
+        });
+        final RouteMapEventFragment routeMapEventFragment = new RouteMapEventFragment();
+        findViewById(R.id.OkBtnAdd).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                ShowFragment(routeMapEventFragment);
+            }
+        });
+        findViewById(R.id.CancelBtnAdd).setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    private void ShowFragment(RouteMapEventFragment routeMapEventFragment) {
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.setCustomAnimations(R.anim.push_left_in, R.anim.push_left_out);
+
+        ft.replace(R.id.activity_add_event, routeMapEventFragment).addToBackStack("MapFragment");
+
+// Start the animated transition.
+        ft.commit();
     }
 
 
+    protected void onActivityResult(int requestCode,int resultCode,Intent data) {
+        if(requestCode == PLACE_PICKER_REQUEST) {
+            if(resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                String address = String.format("%s",place.getAddress());
+                LatLng startLocation = place.getLatLng();
+                StartAddress.setText(address);
+            }
+        } else if(requestCode == PLACE_DESTINATION_PICKER_REQUEST) {
+            if(resultCode == RESULT_OK) {
+                Place place = PlacePicker.getPlace(data, this);
+                String address = String.format("%s",place.getAddress());
+                LatLng DesLocation = place.getLatLng();
+                EndAddress.setText(address);
+            }
+        }
+    }
 
     @Override
     protected void onResume() {
@@ -123,5 +215,13 @@ public class AddEventActivity extends AppCompatActivity implements DatePickerDia
         String minuteString = minute < 10 ? "0"+minute : ""+minute;
         String time = hourString+":"+minuteString;
         timeView.setText(time);
+    }
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() > 0) {
+            getFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
